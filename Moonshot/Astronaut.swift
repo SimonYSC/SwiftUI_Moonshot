b99cb116efae4949194dfda1add7f18d4199f813
Simon Chao <ysc406@nyu.edu>
2021-08-05 11:52:49 -0400

    Create Moonshot app.

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by YunShou Chao on 8/5/21.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
